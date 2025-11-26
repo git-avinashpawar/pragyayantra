80b78dbf1ed826d3caedc7728283ad82650351d5
Avinash Pawar <32817851+git-avinashpawar@users.noreply.github.com>
2025-11-26 11:23:15 +0530

    Update code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -190,7 +190,7 @@ async function getResponse(message) {
   const fileManager = new GoogleAIFileManager(AI_ABCD);
 
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
     tools: [
       {
         codeExecution: {},
